26ad8e0adb970834d237adb06ab139488545b625
Ilakiab08 <142892200+Ilakiab08@users.noreply.github.com>
2024-01-24 00:57:29 +0100

    card

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,22 @@ import Counter from '/src/Counter';
 import Filter from '/src/Filter'; 
 import './App.css';
 import React from 'react';
+import Card from "./assets/Card"
 
 const App = () => {
   return (
     <div>
       <Counter />
       <Filter />
+      <Card 
+                img="katie-zaferes.png"
+                rating="5.0"
+                reviewCount={6}
+                country="USA"
+                title="Life Lessons with Katie Zaferes"
+                price={136}
+            />
+            <card/>
     </div>
   );
 };
